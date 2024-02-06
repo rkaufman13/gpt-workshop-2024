be1aa47dde1ec384a4216b71ab4e7bd2b3b65525
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2024-02-06 13:49:41 -0800

    delete vision stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import Link from "next/link";
+import Chat from "./chat/page";
 
 export default function Home() {
   return (
     <div className="page-container">
-      <h1>Welcome to the GPT Workshop</h1>
-      <p>This repo contains the demo code for a React/NextJS application</p>
-      <p>You can go to the <Link href="/chat">chat example</Link> to see a demo chatbot</p>
-      <p>You can go to the <Link href="/vision">vision example</Link> to see a demo using the vision API</p>
+      <h1>
+        Chat G&T
+        <img src="../assets/martini.png" />
+      </h1>
+      <p>What's to drink?</p>
+      <Chat></Chat>
     </div>
   );
 }
-
